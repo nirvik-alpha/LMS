c452f153296b42e1d8ef30df4518b587e3c1b587
Sadid Rafan <sadidrafan094@gmail.com>
2025-05-19 20:48:09 +0600

    define presentation layer,view,delete,update,add books

--- src/main/java/com/application/lms/LmsApplication.java
+++ src/main/java/com/application/lms/LmsApplication.java
@@ -23,11 +23,10 @@ public class LmsApplication {
 	 */
 
 	@Bean  // Creates a bean to run initialization logic at application startup
-	public CommandLineRunner intialCreate(BookService bookService){
-		return(args)->{
-
-			Book book1 = new Book("ABC","Book Name" , "My First Book");
-			Author author1 = new Author("Test name1" , "Test description");
+	public CommandLineRunner initialCreate(BookService bookService) {
+		return (args) -> {
+			Book book1 = new Book("ABC", "Book name", "My first book");
+			Author author1 = new Author("Test name1", "Test description");
 			Category category1 = new Category("Business books");
 			Publisher publisher1 = new Publisher("First Publisher");
 			book1.addAuthor(author1);
@@ -35,24 +34,23 @@ public class LmsApplication {
 			book1.addPublisher(publisher1);
 			bookService.createBook(book1);
 
-			Book book2 = new Book("ABC2","Book Name2" , "My First Book2");
-			Author author2 = new Author("Test name2" , "Test description2");
-			Category category2 = new Category("Business books2");
-			Publisher publisher2 = new Publisher("First Publisher2");
+			Book book2 = new Book("ABC1", "Book name", "My first book");
+			Author author2 = new Author("Test name2", "Test description");
+			Category category2 = new Category("Science books");
+			Publisher publisher2 = new Publisher("Second Publisher");
 			book2.addAuthor(author2);
 			book2.addCategory(category2);
 			book2.addPublisher(publisher2);
 			bookService.createBook(book2);
 
-			Book book3 = new Book("ABC3","Book Name3" , "My First Book3");
-			Author author3 = new Author("Test name13" , "Test description3");
-			Category category3 = new Category("Business books3");
-			Publisher publisher3 = new Publisher("First Publisher3");
+			Book book3 = new Book("ABC21", "Book name", "My first book");
+			Author author3 = new Author("Test name3", "Test description");
+			Category category3 = new Category("Fiction books");
+			Publisher publisher3 = new Publisher("Third Publisher");
 			book3.addAuthor(author3);
 			book3.addCategory(category3);
 			book3.addPublisher(publisher3);
 			bookService.createBook(book3);
 		};
 	}
-
 }
--- src/main/java/com/application/lms/service/BookService.java
+++ src/main/java/com/application/lms/service/BookService.java
@@ -13,12 +13,12 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public List<Book> findAllBooks(){
+    public List<Book> findAllBooks() {
         return bookRepository.findAll();
     }
 
-    public Book findBookById(Long id){
-        Book book = bookRepository.findById(id).orElseThrow(()->new RuntimeException("Book not found"));
+    public Book findBookById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
         return book;
     }
 
@@ -26,9 +26,12 @@ public class BookService {
         bookRepository.save(book);
     }
 
-    public void deleteBook(Long id){
-        Book book = bookRepository.findById(id).orElseThrow(()->new RuntimeException("Book not found"));
-        bookRepository.deleteById(book.getId());
+    public void updateBook(Book book){
+        bookRepository.save(book);
     }
 
+    public void deleteBook(Long id){
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        bookRepository.deleteById(book.getId());
+    }
 }
